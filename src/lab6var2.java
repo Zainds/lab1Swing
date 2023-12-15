55a4752aee844495f886fef7eea41bcb77026d86
Ангальдт Михаил <beat1097@mail.ru>
2023-12-15 22:39:09 +0700

    уже лучше

--- src/lab6var2.java
+++ src/lab6var2.java
@@ -4,30 +4,50 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.regex.*;
 
-public class lab6var2 extends JFrame {
+public class lab6var2 {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(TextEditor::new);
+    }
+}
+
+class TextEditor extends JFrame {
     private JTextPane textPane;
     private SimpleAttributeSet highlightStyle;
     private Pattern pattern;
 
-    public lab6var2() {
+    public TextEditor() {
         textPane = new JTextPane();
 
         // Задаем строку текстового поля в коде
-        String initialText = "This is a Test. And Another Test. One More Test.";
+        String initialText = "This is a Test. And Another Test. One More Test. gdfg fgfdg dfgfdg dfgfd";
         textPane.setText(initialText);
 
         textPane.setFont(new Font("Serif", Font.PLAIN, 12));
         textPane.addKeyListener(new KeyAdapter() {
             public void keyPressed(KeyEvent e) {
-                if (e.getKeyCode() == KeyEvent.VK_F2) {
-                    // Заменяем выделенные слова, к которым подведена каретка
-                    int start = textPane.getSelectionStart();
-                    int end = textPane.getSelectionEnd();
-                    String toReplace = textPane.getText().substring(start, end);
-                    String replaced = toReplace.replaceAll("\\b([A-Z]\\w+) ([A-Z]\\w+)\\b", "$1-$2");
-                    textPane.replaceSelection(replaced);
-                } else if (e.getKeyCode() == KeyEvent.VK_F3) {
-                    removeHighlight();
+                int caretPos = textPane.getCaretPosition();
+                String text = textPane.getText();
+                Matcher matcher = pattern.matcher(text);
+                while (matcher.find()) {
+                    int start = matcher.start();
+                    int end = matcher.end();
+                    String[] words = text.substring(start, end).split(" ");
+                    if (caretPos >= start && caretPos <= start + words[0].length() ||
+                            caretPos >= start + words[0].length() + 1 && caretPos <= end) {
+                        if (e.getKeyCode() == KeyEvent.VK_F2) {
+                            // Заменяем выделенные слова, к которым подведена каретка
+                            String replaced = text.substring(start, end).replaceAll("\\b([A-Z]\\w+) ([A-Z]\\w+)\\b", "$1-$2");
+                            textPane.select(start, end);
+                            textPane.replaceSelection(replaced);
+                            // Снимаем выделение
+                            textPane.setCharacterAttributes(new SimpleAttributeSet(), true);
+                            break;
+                        } else if (e.getKeyCode() == KeyEvent.VK_F3) {
+                            // Снимаем выделение
+                            textPane.setCharacterAttributes(new SimpleAttributeSet(), true);
+                            break;
+                        }
+                    }
                 }
             }
         });
@@ -48,11 +68,6 @@ public class lab6var2 extends JFrame {
         highlightText();
     }
 
-    private void removeHighlight() {
-        StyledDocument doc = textPane.getStyledDocument();
-        doc.setCharacterAttributes(0, doc.getLength(), new SimpleAttributeSet(), true);
-    }
-
     private void highlightText() {
         StyledDocument doc = textPane.getStyledDocument();
         String text = textPane.getText();
@@ -62,9 +77,5 @@ public class lab6var2 extends JFrame {
             int end = matcher.end();
             doc.setCharacterAttributes(start, end - start, highlightStyle, false);
         }
-    }
-
-    public static void main(String[] args) {
-        new lab6var2();
     }
 }
