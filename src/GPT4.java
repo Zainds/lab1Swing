4f87826ff5e406e34a7f6555a9c1266a05ee1942
Ангальдт Михаил <beat1097@mail.ru>
2024-01-22 17:58:53 +0700

    lab 4 t1+t2 gotov

--- src/GPT4.java
+++ src/GPT4.java
@@ -1,4 +1,3 @@
-
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
@@ -6,60 +5,70 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class GPT4 extends JFrame {
-private JTable matrixTable;
-private JTextField textField;
-private JButton updateButton;
-private DefaultTableModel tableModel;
-private double[][] matrix;
+        private JTable matrixTable;
+        private JTextField textField;
+        private JButton updateButton;
+        private JButton modifyButton;
+        private DefaultTableModel tableModel;
+        private double[][] matrix;
 
-public GPT4(int n) {
-        setTitle("Матрица");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new BorderLayout());
+        public GPT4(int n) {
+                setTitle("Матрица");
+                setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                setLayout(new BorderLayout());
 
-        matrix = createMatrix(n);
+                matrix = createMatrix(n);
 
-        tableModel = new DefaultTableModel(n, n);
-        matrixTable = new JTable(tableModel);
-        matrixTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        matrixTable.setColumnSelectionAllowed(false);
-        matrixTable.setRowSelectionAllowed(false);
-        matrixTable.setCellSelectionEnabled(true);
-        matrixTable.setShowGrid(true);
+                tableModel = new DefaultTableModel(n, n);
+                matrixTable = new JTable(tableModel);
+                matrixTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+                matrixTable.setColumnSelectionAllowed(false);
+                matrixTable.setRowSelectionAllowed(false);
+                matrixTable.setCellSelectionEnabled(true);
+                matrixTable.setShowGrid(true);
 
-        updateTable();
+                updateTable();
 
-        JScrollPane scrollPane = new JScrollPane(matrixTable);
-        add(scrollPane, BorderLayout.CENTER);
+                JScrollPane scrollPane = new JScrollPane(matrixTable);
+                add(scrollPane, BorderLayout.CENTER);
 
-        JPanel panel = new JPanel(new FlowLayout());
+                JPanel panel = new JPanel(new FlowLayout());
 
-        textField = new JTextField(10);
-        panel.add(textField);
+                textField = new JTextField(10);
+                panel.add(textField);
 
-        updateButton = new JButton("Занести на место");
-        updateButton.addActionListener(new ActionListener() {
-@Override
-public void actionPerformed(ActionEvent e) {
-        updateMatrix();
+                updateButton = new JButton("Занести на место");
+                updateButton.addActionListener(new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                                updateMatrix();
+                        }
+                });
+                panel.add(updateButton);
+
+                modifyButton = new JButton("Modify Columns");
+                modifyButton.addActionListener(new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                                modifyColumns();
+                        }
+                });
+                panel.add(modifyButton);
+
+                add(panel, BorderLayout.SOUTH);
+
+                matrixTable.getSelectionModel().addListSelectionListener(e -> {
+                        int selectedRow = matrixTable.getSelectedRow();
+                        int selectedColumn = matrixTable.getSelectedColumn();
+                        if (selectedRow >= 0 && selectedColumn >= 0) {
+                                double value = (double) matrixTable.getValueAt(selectedRow, selectedColumn);
+                                textField.setText(String.valueOf(value));
+                        }
+                });
+
+                pack();
+                setVisible(true);
         }
-        });
-        panel.add(updateButton);
-
-        add(panel, BorderLayout.SOUTH);
-
-        matrixTable.getSelectionModel().addListSelectionListener(e -> {
-        int selectedRow = matrixTable.getSelectedRow();
-        int selectedColumn = matrixTable.getSelectedColumn();
-        if (selectedRow >= 0 && selectedColumn >= 0) {
-        double value = (double) matrixTable.getValueAt(selectedRow, selectedColumn);
-        textField.setText(String.valueOf(value));
-        }
-        });
-
-        pack();
-        setVisible(true);
-}
 
         private double[][] createMatrix(int n) {
                 double[][] matrix = new double[n][n];
@@ -77,33 +86,44 @@ public void actionPerformed(ActionEvent e) {
                 return matrix;
         }
 
-
         private void updateTable() {
-        for (int i = 0; i < matrix.length; i++) {
-                for (int j = 0; j < matrix[i].length; j++) {
-                        matrixTable.setValueAt(matrix[i][j], i, j);
+                for (int i = 0; i < matrix.length; i++) {
+                        for (int j = 0; j < matrix[i].length; j++) {
+                                matrixTable.setValueAt(matrix[i][j], i, j);
+                        }
                 }
         }
-}
 
-private void updateMatrix() {
-        int selectedRow = matrixTable.getSelectedRow();
-        int selectedColumn = matrixTable.getSelectedColumn();
-        if (selectedRow >= 0 && selectedColumn >= 0) {
-        String valueStr = textField.getText();
-        try {
-                double value = Double.parseDouble(valueStr);
-                matrix[selectedRow][selectedColumn] = value;
-                tableModel.setValueAt(value, selectedRow, selectedColumn);
-        }
-        catch (NumberFormatException e) {
-        JOptionPane.showMessageDialog(this, "Неверное значение", "Ошибка", JOptionPane.ERROR_MESSAGE);
-        }
-        }
+        private void updateMatrix() {
+                int selectedRow = matrixTable.getSelectedRow();
+                int selectedColumn = matrixTable.getSelectedColumn();
+                if (selectedRow >= 0 && selectedColumn >= 0) {
+                        String valueStr = textField.getText();
+                        try {
+                                double value = Double.parseDouble(valueStr);
+                                matrix[selectedRow][selectedColumn] = value;
+                                tableModel.setValueAt(value, selectedRow, selectedColumn);
+                        } catch (NumberFormatException e) {
+                                JOptionPane.showMessageDialog(this, "Неверное значение", "Ошибка", JOptionPane.ERROR_MESSAGE);
+                        }
+                }
         }
 
-public static void main(String[] args) {
-        int n = 6; // Заданное значение n
-        SwingUtilities.invokeLater(() -> new GPT4(n));
+        private void modifyColumns() {
+                for (int i = 0; i < matrix.length; i++) {
+                        for (int j = 0; j < matrix[i].length; j++) {
+                                if (j % 2 == 0) {
+                                        matrix[i][j] += 3;
+                                } else {
+                                        matrix[i][j] -= 3;
+                                }
+                        }
+                }
+                updateTable();
+        }
+
+        public static void main(String[] args) {
+                int n = 6; // Заданное значение n
+                SwingUtilities.invokeLater(() -> new GPT4(n));
         }
 }
